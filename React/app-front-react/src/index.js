a40e64e34f0370b6bca0069429109a83eef8a983
José Javier <jotatributo4@gmail.com>
2020-02-12 17:13:07 +0100

    sasas

--- React/app-front-react/src/index.js
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './componentes/app';
-
-ReactDOM.render(
-    <App/>,
-    document.getElementById("contenedorRaiz")
-);
